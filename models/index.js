9ab1c6185063b75bf00b9c1ed760c1ae9e99a994
matthewbarnes1 <matthew.barnes35@gmail.com>
2023-09-27 18:54:11 -0400

    Setup model associations in index.js models file

--- models/index.js
+++ models/index.js
@@ -6,30 +6,32 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'driver_id',
-  onDelete: 'CASCADE',
+  as: 'product_category'
 });
 
+// Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'driver_id',
+  as: 'category_product'
 });
 
+// Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'product_tags'
+});
 
+
+// Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-
-
-// ! You'll need to execute association methods on your Sequelize models to create the following relationships between them:
-
-// TODO: Product belongs to Category, and Category has many Product models, as a category can have multiple products but a product can only belong to one category.
-//* One to many
-
-
-// TODO: Product belongs to many Tag models, and Tag belongs to many Product models. Allow products to have multiple tags and tags to have many products by using the ProductTag through model.
-//* Many to many 
-
-// TODO: Hint: Make sure you set up foreign key relationships that match the column we created in the respective models.
-
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'tag_product'
+});
 
 module.exports = {
   Product,
