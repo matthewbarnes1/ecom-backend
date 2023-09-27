01988012769c65541c199cba54a07936876d6c3e
matthewbarnes1 <matthew.barnes35@gmail.com>
2023-09-27 18:29:32 -0400

    Created neccesary tables to setup my schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,31 @@
 DROP DATABASE IF EXISTS ecommerce_db;
-
 CREATE DATABASE ecommerce_db;
+USE ecommerce_db;
+
+
+CREATE TABLE Category (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  category_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE Product( 
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  product_name VARCHAR(30) NOT NULL,
+  price DECIMAL(10,2) NOT NULL,
+  stock INT NOT NULL DEFAULT 10,
+--   ! Check to see if this is correct for referencing the category id
+  category_id REFERENCES Category(id)
+)
+
+CREATE TABLE Tag (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  tag_name VARCHAR(30)
+);
+
+CREATE TABLE ProductTag (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  product_id REFERENCES Product(id),
+  tag_id REFERENCES Tag(id)
+);
+
+
