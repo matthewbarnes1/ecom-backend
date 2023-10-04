e2743d9d7a27960ff626b0d573b2fbaae0a62e7e
matthewbarnes1 <matthew.barnes35@gmail.com>
2023-10-04 17:51:33 -0400

    put route redo

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,16 +1,6 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
-router.post('/categories', async (req, res) => {
-  try {
-    const newCategory = await Category.create(req.body);
-    res.status(201).json(newCategory);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-
 router.get('/', async (req, res) => {
   try {
     const categories = await Category.findAll({
@@ -20,64 +10,6 @@ router.get('/', async (req, res) => {
       }
     });
     res.json(categories);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-router.post('/', async (req, res) => {
-  try {
-    const newProduct = await Product.create(req.body);
-    if (req.body.tagIds.length) {
-      const productTagIdArr = req.body.tagIds.map((tag_id) => {
-        return {
-          product_id: newProduct.id,
-          tag_id,
-        };
-      });
-      const productTags = await ProductTag.bulkCreate(productTagIdArr);
-      res.status(201).json(newProduct);
-    } else {
-      res.status(200).json(newProduct);
-    }
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-router.put('/categories/:id', async (req, res) => {
-  try {
-    const updatedCategory = await Category.update(req.body, {
-      where: {
-        id: req.params.id,
-      },
-    });
-
-    if (!updatedCategory) {
-      res.status(404).json({ message: 'No category found with this id!' });
-      return;
-    }
-
-    res.json(updatedCategory);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-router.delete('/categories/:id', async (req, res) => {
-  try {
-    const deletedCategory = await Category.destroy({
-      where: {
-        id: req.params.id,
-      },
-    });
-
-    if (!deletedCategory) {
-      res.status(404).json({ message: 'No category found with this id!' });
-      return;
-    }
-
-    res.json({ message: 'Category deleted successfully!' });
   } catch (err) {
     res.status(500).json(err);
   }
