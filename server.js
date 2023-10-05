f2a2a373c6d4eadf913faec28d235e71920c2612
matthewbarnes1 <matthew.barnes35@gmail.com>
2023-10-04 21:38:30 -0400

    working

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {  
   logging: console.log
   app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
