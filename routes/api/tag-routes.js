ab4c4b571e436baa69e891a6bbcd7dc9a063eb1d
matthewbarnes1 <matthew.barnes35@gmail.com>
2023-10-04 21:37:56 -0400

    added tag CRUD routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -35,4 +35,51 @@ router.get('/:id', async (req, res) => {
   }
 });
 
+router.post('/', async (req, res) => {
+  try {
+    const newTag = await Tag.create(req.body);
+    res.status(201).json(newTag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedTag = await Tag.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!updatedTag[0]) {
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
+    }
+
+    res.json({ message: 'Tag updated successfully!' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedTag = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!deletedTag) {
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
+    }
+
+    res.json({ message: 'Tag deleted successfully!' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
